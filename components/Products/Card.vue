da39f2c53795e91959d19a57da5614068e9c66b6
Jesse <jessemerge2@gmail.com>
2023-09-18 22:06:26 -0700

    ….

--- components/Products/Card.vue
+++ components/Products/Card.vue
@@ -1,7 +1,14 @@
 <template>
-	<div>Card</div>
+	<div>
+		<p>{{ product.name }}</p>
+		<p>{{ product.id }}</p>
+	</div>
 </template>
 
-<script setup></script>
+<script setup>
+	const props = defineProps({
+		product: Object,
+	});
+</script>
 
 <style lang="scss" scoped></style>
