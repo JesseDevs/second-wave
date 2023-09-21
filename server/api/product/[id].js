3688784ff7d3847f77fb1dce00ae374e0bf97eee
Jesse <jessemerge2@gmail.com>
2023-09-21 10:06:44 -0700

    primsa and data pushes with api

---
+++ server/api/product/[id].js
@@ -0,0 +1,17 @@
+import products from '@/data/products.json';
+export default defineEventHandler((event) => {
+	const { id } = event.context.params;
+
+	const product = products.find((p) => {
+		return p.id === parseInt(id);
+	});
+
+	if (!product) {
+		throw createError({
+			statusCode: 404,
+			statusMessage: `Product ${id} does not exist`,
+		});
+	}
+
+	return product;
+});
--- server/api/products/[item].js
+++ server/api/products/[item].js
@@ -1,20 +1,69 @@
 import products from '@/data/products.json';
 
 export default defineEventHandler((event) => {
-	// const { item } = event.context.params;
-	// const { brand, minPrice, maxPrice } = getQuery(event);
+	const { item } = event.context.params;
+	const { brand, gender, type, minPrice, maxPrice } = getQuery(event);
+
+	let filteredProducts = products;
+
+	if (item === 'all') {
+		return (filteredProducts = products);
+	}
+
+	filteredProducts = filteredProducts.filter((product) => {
+		return product.name.toLowerCase().includes(item.toLowerCase());
+	});
+
+	// if (brand) {
+	// 	filteredProducts = filteredProducts.filter((product) => {
+	// 		return product.brand.toLowerCase() === brand.toLowerCase();
+	// 	});
+	// }
+
+	// if (gender) {
+	// 	filteredProducts = filteredProducts.filter((product) => {
+	// 		return product.gender.toLowerCase() === gender.toLowerCase();
+	// 	});
+	// }
+
+	// if (type) {
+	// 	filteredProducts = filteredProducts.filter((product) => {
+	// 		return product.type.toLowerCase() === type.toLowerCase();
+	// 	});
+	// }
+
+	// if (minPrice) {
+	// 	filteredProducts.filter((product) => {
+	// 		return product.price >= parseInt(minPrice);
+	// 	});
+	// }
+
+	// if (maxPrice) {
+	// 	filteredProducts.filter((product) => {
+	// 		return product.price <= parseInt(maxPrice);
+	// 	});
+	// }
 
 	// const filters = {
-	// 	city: city.toLowerCase(),
+	// 	item: item.toLowerCase(),
 	// };
 
-	// if (make) {
-	// 	filters.make = make;
+	// if (brand) {
+	// 	filters.brand = brand;
+	// }
+
+	// if (gender) {
+	// 	filters.gender = gender;
+	// }
+
+	// if (type) {
+	// 	filters.type = type;
 	// }
 
 	// if (minPrice || maxPrice) {
 	// 	filters.price = {};
 	// }
+
 	// if (minPrice) {
 	// 	filters.price.gte = parseInt(minPrice);
 	// }
@@ -22,8 +71,9 @@ export default defineEventHandler((event) => {
 	// if (maxPrice) {
 	// 	filters.price.lte = parseInt(minPrice);
 	// }
+
 	// return prisma.car.findMany({
 	// 	where: filters,
 	// });
-	return products;
+	return filteredProducts;
 });
